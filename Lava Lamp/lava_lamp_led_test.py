c0e071428880d7e432e17a5e0516570a92efede9
zarify <spam@headtilt.me>
2019-03-11 07:35:51 +0800

    Create lava_lamp_led_test.py
    Initial commit

---
+++ Lava Lamp/lava_lamp_led_test.py
@@ -0,0 +1,56 @@
+from microbit import *
+import neopixel
+from random import randint
+
+np = neopixel.NeoPixel(pin12, 1)
+h, s, v = (0.0, 1.0, 0.5)
+targ = v
+direction = 1
+step = 0.001
+
+def hsv_to_rgb(h, s, v):
+  # Algorithm from: https://stackoverflow.com/a/26856771
+  # Expects floats for all 3 arguments, so hue space is 0-1.
+  if s == 0.0:
+    v*=255
+    return (v, v, v)
+  i = int(h*6.) # XXX assume int() truncates!
+  f = (h*6.)-i
+  p,q,t = int(255*(v*(1.-s))), int(255*(v*(1.-s*f))), int(255*(v*(1.-s*(1.-f))))
+  v*=255
+  i%=6
+  if i == 0: return (v, t, p)
+  if i == 1: return (q, v, p)
+  if i == 2: return (p, v, t)
+  if i == 3: return (p, q, v)
+  if i == 4: return (t, p, v)
+  if i == 5: return (v, p, q)
+
+def get_new_trend(v):
+  #
+  tl = randint(1, 4) / 10
+  # choose direction for trend (-ve dimmer, +ve brighter)
+  if (v + tl) >= 1.0:
+    td = -1
+  elif (v - tl) <= 0.1:
+    td = 1
+  else:
+    td = -1 if randint(1,2) == 1 else 1
+  to = round(v + tl * td, 1)
+
+  return (to, td) # return the new target vibrance and direction
+
+while True:
+  a = button_a.was_pressed()
+  b = button_b.was_pressed()
+
+  h = (h + 0.001) % 1 # cycle through hue space
+  if (direction == -1 and v >= targ) or (direction == 1 and v <= targ):
+    v += step * direction
+  else:
+    targ, direction = get_new_trend(v)
+
+  (r, g, b) = hsv_to_rgb(h, s, v)
+  np[0] = (int(r), int(g), int(b))
+  np.show()
+  sleep(50)
